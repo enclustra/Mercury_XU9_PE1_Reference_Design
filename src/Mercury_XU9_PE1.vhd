dd44ee713397bdef123035923f6e2d9a3aedabf7
jenkins-enclustra <info@enclustra.com>
2020-11-12 13:18:15 +0100

    Updated github repository with reference design version: 1.0.0

---
+++ src/Mercury_XU9_PE1.vhd
@@ -0,0 +1,232 @@
+---------------------------------------------------------------------------------------------------
+-- Copyright (c) 2020 by Enclustra GmbH, Switzerland.
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of
+-- this hardware, software, firmware, and associated documentation files (the
+-- "Product"), to deal in the Product without restriction, including without
+-- limitation the rights to use, copy, modify, merge, publish, distribute,
+-- sublicense, and/or sell copies of the Product, and to permit persons to whom the
+-- Product is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Product.
+--
+-- THE PRODUCT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+-- INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+-- PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+-- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+-- OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+-- PRODUCT OR THE USE OR OTHER DEALINGS IN THE PRODUCT.
+---------------------------------------------------------------------------------------------------
+
+---------------------------------------------------------------------------------------------------
+-- libraries
+---------------------------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+---------------------------------------------------------------------------------------------------
+-- entity declaration
+---------------------------------------------------------------------------------------------------
+entity Mercury_XU9_PE1 is
+  generic (
+    BG_WIDTH : natural
+  );
+  
+  port (
+    
+    -- Anios_A
+    IOA_D0_P                       : inout   std_logic;
+    IOA_D1_N                       : inout   std_logic;
+    IOA_D2_P                       : inout   std_logic;
+    IOA_D3_N                       : inout   std_logic;
+    IOA_D4_P                       : inout   std_logic;
+    IOA_D5_N                       : inout   std_logic;
+    IOA_D6_P                       : inout   std_logic;
+    IOA_D7_N                       : inout   std_logic;
+    IOA_D8_P                       : inout   std_logic;
+    IOA_D9_N                       : inout   std_logic;
+    IOA_D10_P                      : inout   std_logic;
+    IOA_D11_N                      : inout   std_logic;
+    IOA_D12_P                      : inout   std_logic;
+    IOA_D13_N                      : inout   std_logic;
+    IOA_D14_P                      : inout   std_logic;
+    IOA_D15_N                      : inout   std_logic;
+    IOA_D16_P                      : inout   std_logic;
+    IOA_D17_N                      : inout   std_logic;
+    IOA_D18_P                      : inout   std_logic;
+    IOA_D19_N                      : inout   std_logic;
+    IOA_D20_P                      : inout   std_logic;
+    IOA_D21_N                      : inout   std_logic;
+    IOA_D22_P                      : inout   std_logic;
+    IOA_D23_N                      : inout   std_logic;
+    IOA_CLK1_N                     : inout   std_logic;
+    IOA_CLK0_P                     : inout   std_logic;
+    
+    -- Anios_B
+    IOB_D0_P                       : inout   std_logic;
+    IOB_D1_N                       : inout   std_logic;
+    IOB_D2_P                       : inout   std_logic;
+    IOB_D3_N                       : inout   std_logic;
+    IOB_D4_P                       : inout   std_logic;
+    IOB_D5_N                       : inout   std_logic;
+    IOB_D6_P                       : inout   std_logic;
+    IOB_D7_N                       : inout   std_logic;
+    IOB_D8_P                       : inout   std_logic;
+    IOB_D9_N                       : inout   std_logic;
+    IOB_D10_P                      : inout   std_logic;
+    IOB_D11_N                      : inout   std_logic;
+    IOB_D12_P                      : inout   std_logic;
+    IOB_D13_N                      : inout   std_logic;
+    IOB_D14_P                      : inout   std_logic;
+    IOB_D15_N                      : inout   std_logic;
+    IOB_D16_SC0_DIP1_N             : inout   std_logic;
+    IOB_D17_SC1_DIP2_N             : inout   std_logic;
+    IOB_D20_SC4_BTN0_N             : inout   std_logic;
+    IOB_D21_SC5_BTN1_N             : inout   std_logic;
+    IOB_D22_SC6_BTN2_N             : inout   std_logic;
+    IOB_D23_SC7_BTN3_N             : inout   std_logic;
+    IOB_CLK1_N                     : inout   std_logic;
+    IOB_CLK0_P                     : inout   std_logic;
+    
+    -- FMC0
+    FMC_HA06_N                     : inout   std_logic;
+    FMC_HA06_P                     : inout   std_logic;
+    FMC_HA09_N                     : inout   std_logic;
+    FMC_HA09_P                     : inout   std_logic;
+    FMC_HA10_N                     : inout   std_logic;
+    FMC_HA10_P                     : inout   std_logic;
+    FMC_HA13_N                     : inout   std_logic;
+    FMC_HA13_P                     : inout   std_logic;
+    FMC_HB01_N                     : inout   std_logic;
+    FMC_HB01_P                     : inout   std_logic;
+    FMC_HB02_N                     : inout   std_logic;
+    FMC_HB02_P                     : inout   std_logic;
+    FMC_HB03_N                     : inout   std_logic;
+    FMC_HB03_P                     : inout   std_logic;
+    FMC_HB04_N                     : inout   std_logic;
+    FMC_HB04_P                     : inout   std_logic;
+    FMC_HB05_N                     : inout   std_logic;
+    FMC_HB05_P                     : inout   std_logic;
+    FMC_HB07_N                     : inout   std_logic;
+    FMC_HB07_P                     : inout   std_logic;
+    FMC_HB09_N                     : inout   std_logic;
+    FMC_HB09_P                     : inout   std_logic;
+    FMC_HA00_CC_N                  : inout   std_logic;
+    FMC_HA00_CC_P                  : inout   std_logic;
+    FMC_HB00_CC_N                  : inout   std_logic;
+    FMC_HB00_CC_P                  : inout   std_logic;
+    
+    -- I2C_PL
+    I2C_SCL_PL                     : inout   std_logic;
+    I2C_SDA_PL                     : inout   std_logic;
+    
+    -- LED
+    PL_LED2_N                      : out     std_logic;
+    
+    -- PE1_SI5338_CLK3
+    OSC_N                          : in      std_logic;
+    OSC_P                          : in      std_logic;
+    
+    -- PL_100_MHz_Oscillator
+    CLK100_PL_N                    : in      std_logic;
+    CLK100_PL_P                    : in      std_logic;
+    
+    -- PL_DDR4_Memory
+    DDR4PL_ACT_N                   : out     std_logic;
+    DDR4PL_RST_N                   : out     std_logic;
+    DDR4PL_BA                      : out     std_logic_vector(1 downto 0);
+    DDR4PL_BG                      : out     std_logic_vector(1 downto 0);
+    DDR4PL_CKE                     : out     std_logic_vector(0 downto 0);
+    DDR4PL_DQ                      : inout   std_logic_vector(63 downto 0);
+    DDR4PL_ODT                     : out     std_logic_vector(0 downto 0);
+    DDR4PL_A                       : out     std_logic_vector(16 downto 0);
+    DDR4PL_CK_N                    : out     std_logic_vector(0 downto 0);
+    DDR4PL_CK_P                    : out     std_logic_vector(0 downto 0);
+    DDR4PL_CS_N                    : out     std_logic_vector(0 downto 0);
+    DDR4PL_DM                      : inout   std_logic_vector(7 downto 0);
+    DDR4PL_DQS_P                   : inout   std_logic_vector(7 downto 0);
+    DDR4PL_DQS_N                   : inout   std_logic_vector(7 downto 0)
+  );
+end Mercury_XU9_PE1;
+
+architecture rtl of Mercury_XU9_PE1 is
+
+  ---------------------------------------------------------------------------------------------------
+  -- component declarations
+  ---------------------------------------------------------------------------------------------------
+  component Mercury_XU9 is
+    port (
+      Clk100              : out    std_logic;
+      Clk50               : out    std_logic;
+      Rst_N               : out    std_logic;
+      C0_SYS_CLK_clk_n    : in     std_logic;
+      C0_SYS_CLK_clk_p    : in     std_logic;
+      C0_DDR4_act_n       : out    std_logic;
+      C0_DDR4_reset_n     : out    std_logic;
+      C0_DDR4_ba          : out    std_logic_vector(1 downto 0);
+      C0_DDR4_bg          : out    std_logic_vector(BG_WIDTH-1 downto 0);
+      C0_DDR4_cke         : out    std_logic_vector(0 downto 0);
+      C0_DDR4_dq          : inout  std_logic_vector(63 downto 0);
+      C0_DDR4_odt         : out    std_logic_vector(0 downto 0);
+      C0_DDR4_adr         : out    std_logic_vector(16 downto 0);
+      C0_DDR4_ck_c        : out    std_logic_vector(0 downto 0);
+      C0_DDR4_ck_t        : out    std_logic_vector(0 downto 0);
+      C0_DDR4_cs_n        : out    std_logic_vector(0 downto 0);
+      C0_DDR4_dm_n        : inout  std_logic_vector(7 downto 0);
+      C0_DDR4_dqs_c       : inout  std_logic_vector(7 downto 0);
+      C0_DDR4_dqs_t       : inout  std_logic_vector(7 downto 0)
+    );
+    
+  end component Mercury_XU9;
+
+  ---------------------------------------------------------------------------------------------------
+  -- signal declarations
+  ---------------------------------------------------------------------------------------------------
+  signal Clk100           : std_logic;
+  signal Clk50            : std_logic;
+  signal Rst_N            : std_logic;
+  signal LedCount         : unsigned(23 downto 0);
+
+begin
+  
+  ---------------------------------------------------------------------------------------------------
+  -- processor system instance
+  ---------------------------------------------------------------------------------------------------
+  Mercury_XU9_i: component Mercury_XU9
+    port map (
+      Clk100               => Clk100,
+      Clk50                => Clk50,
+      Rst_N                => Rst_N,
+      C0_SYS_CLK_clk_n     => CLK100_PL_N,
+      C0_SYS_CLK_clk_p     => CLK100_PL_P,
+      C0_DDR4_act_n        => DDR4PL_ACT_N,
+      C0_DDR4_reset_n      => DDR4PL_RST_N,
+      C0_DDR4_ba           => DDR4PL_BA,
+      C0_DDR4_bg           => DDR4PL_BG (BG_WIDTH-1 downto 0),
+      C0_DDR4_cke          => DDR4PL_CKE,
+      C0_DDR4_dq           => DDR4PL_DQ,
+      C0_DDR4_odt          => DDR4PL_ODT,
+      C0_DDR4_adr          => DDR4PL_A,
+      C0_DDR4_ck_c         => DDR4PL_CK_N,
+      C0_DDR4_ck_t         => DDR4PL_CK_P,
+      C0_DDR4_cs_n         => DDR4PL_CS_N,
+      C0_DDR4_dm_n         => DDR4PL_DM,
+      C0_DDR4_dqs_c        => DDR4PL_DQS_N,
+      C0_DDR4_dqs_t        => DDR4PL_DQS_P
+    );
+  
+  process (Clk50)
+  begin
+    if rising_edge (Clk50) then
+      if Rst_N = '0' then
+        LedCount    <= (others => '0');
+      else
+        LedCount    <= LedCount + 1;
+      end if;
+    end if;
+  end process;
+  PL_LED2_N <= '0' when LedCount(LedCount'high) = '0' else 'Z';
+
+end rtl;
